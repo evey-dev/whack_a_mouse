39dc3eafa68504e7cee3d0c0713ce5726ae9480f
eli27 <buip@bxscience.edu>
2022-04-29 23:20:51 -0400

    updated startGame() created showMoles()

--- lib/game.dart
+++ lib/game.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:whack_a_mole/widget/mole.dart';
+import 'dart:math';
+
 class Game extends StatefulWidget {
   const Game({Key? key}) : super(key: key);
   @override
@@ -8,34 +11,66 @@ class Game extends StatefulWidget {
 
 class _GameState extends State<Game>{
   List<double> moleLoc = [400.0, 400.0, 450.0, 500.0, 500.0];
+  double speed = 1;
+  int points = 0;
+  int showTime = 3000;
+  Stopwatch s = Stopwatch();
+
+
   late List<Mole> moles;
   _GameState() {
     moles = [
-      Mole(moleLoc[0], moleLoc[0] - 100, 53, const Duration(seconds: 1)),
-      Mole(moleLoc[1], moleLoc[1] - 100, 323, const Duration(seconds: 1)),
-      Mole(moleLoc[2], moleLoc[2] - 100, 188, const Duration(seconds: 1)),
-      Mole(moleLoc[3], moleLoc[3] - 100, 53, const Duration(seconds: 1)),
-      Mole(moleLoc[4], moleLoc[4] - 100, 323, const Duration(seconds: 1)),
+      Mole(moleLoc[0], moleLoc[0] - 100, 53, const Duration(milliseconds: 500)),
+      Mole(moleLoc[1], moleLoc[1] - 100, 323, const Duration(milliseconds: 500)),
+      Mole(moleLoc[2], moleLoc[2] - 100, 188, const Duration(milliseconds: 500)),
+      Mole(moleLoc[3], moleLoc[3] - 100, 53, const Duration(milliseconds: 500)),
+      Mole(moleLoc[4], moleLoc[4] - 100, 323, const Duration(milliseconds: 500)),
     ];
 
     startGame();
   }
+
   void startGame() {
-    moles[0].show();
-    moles[1].show();
-    moles[2].show();
-    moles[3].show();
-    moles[4].show();
+    s.start();
+    Timer t = Timer.periodic(const Duration(milliseconds: 3000), showMoles);
+    for(var i = 0; i < 6; i++) {
+      Timer(Duration(seconds: i == 0 || i == 3 ? 9 : 10),(){
+        t.cancel();
+        t = Timer.periodic(Duration(milliseconds: 3000 - i * 500), showMoles);
+      });
+    }
   }
+
+  void showMoles(timer) {
+    moles[0].hide();
+    moles[1].hide();
+    moles[2].hide();
+    moles[3].hide();
+    moles[4].hide();
+
+    var random = [false,false,false,false,false];
+    Random x = Random();
+    for(var i = 0; i < random.length; i++) {
+      random[i] = x.nextBool();
+      //안영하세요
+    }
+
+    for (var i = 0; i < random.length; i++) {
+      if(random[i]){
+        moles[i].show();
+        print('showing '+i.toString());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold (
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black),
-          image: const DecorationImage(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
             image: AssetImage('game/background.png'),
             fit: BoxFit.fill,
           )
